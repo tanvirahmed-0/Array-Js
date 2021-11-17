6f7f32cafd07b2dbff9267724949140898f32112
Tanvir Ahmed <93043879+tanvirahmed-0@users.noreply.github.com>
2021-11-17 18:42:15 +0600

    Add files via upload

---
+++ New folder/main.js
@@ -0,0 +1,46 @@
+//Dynamic javascript
+let person = {
+    name : 'Tanvir',
+    age : 18
+}
+//Dot Notation
+person.age = 20; 
+
+//Bracket Notation
+person['name'] = 'Tanveer';
+console.log(person.name);
+
+
+//Array
+let myTeam = ['Tanvir', 'Samsung', 'Honor', 'One Plus'];
+myTeam[4] = 'Iphone';
+myTeam[5] = 310;
+console.log(myTeam.length);
+//Performing a task 
+function great(name, lastName) {
+    console.log('Hello' + ' ' + name)
+    console.log('Hello' + ' ' + lastName)
+    
+}
+great('Tanvir')
+great('Mim')
+
+//Calculating a value 
+function addition(number){
+    return number + number
+}
+
+document.write(addition(5))
+
+// ____________________________________________________________________|
+// // (                                                                |
+// //     {              Hey, I am Tanvir Ahmed. A professional coder  |
+// //         [                                                        |
+// //             (                                                    |                                         
+// //                 ==> >----------++++++++++++++-----------< <==    |
+// //             )%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    |
+// //         ]$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$    |
+// //     }********************************************************    |
+// // )++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    |
+// ____________________________________________________________________|
+// --------------------------------------------------------------------|
